71004de9f774d480f510c8e26c624290f6216cc2
Omar Mattos <omarmatt84@gmail.com>
2025-06-25 17:37:00 -0300

    se centro el listgroup

--- src/components/EmpleadoList.jsx
+++ src/components/EmpleadoList.jsx
@@ -3,7 +3,7 @@ import EmpleadoRow from "./EmpleadoRow";
 
 const EmpleadoList = ({empleados}) => {
     return (
-        <section className="d-flex align-column-center">
+        <section className="d-flex justify-content-center">
             <ListGroup>
                 {
                     empleados.map((item, indice)=><EmpleadoRow key={indice} empleado={item}></EmpleadoRow>)
